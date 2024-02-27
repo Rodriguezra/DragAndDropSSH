a787816a3f3cca4a38336579f5103c3552840e9f
MeghanAgarwal <73602963+MeghanAgarwal@users.noreply.github.com>
2024-02-27 15:37:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,25 @@ let Encrycption, CipheredData, publicKey, privateKey, plaintext, decriptedplaint
 let EncrycptionImg, CipheredDataImg, publicKeyImg, privateKeyImg, plaintextImg, decriptedplaintxtImg;
 let center1, center2, center3, center4, center5;
 let screen = 0;
-let widthConstraint, heightConstraint;
+
+//start = 0
+//instructions = 1
+//game = 2
+//restart = 3
+
+
 
 function mousePressed() {
-  if (screen === 1) { // Game1
-    // if we press restart button
+
+  if(screen === 0 ){ //on the start screen
     if (mouseX > width / 2 - 50 && mouseX < width / 2 + 50 && mouseY > height / 2 + 50 && mouseY < height / 2 + 90) {
-      screen = 0;
-      // Reset card positions to initial locations
+      showInstructionScreen();
+      screen = 1;
+      }
+  } else if(screen === 1 || screen === 3){// if on the instructions or restart screen
+    //press begin button or restart button pressed
+    if (mouseX > width / 2 - 50 && mouseX < width / 2 + 50 && mouseY > height / 2 + 50 && mouseY < height / 2 + 90) {
+      screen = 2;
       CipheredData.position = createVector(width / 4, height - (height / 3));
       publicKey.position = createVector(width / 4, height - (height / 3) + 50);
       privateKey.position = createVector(width / 2, height - (height / 3) - 50);
@@ -19,13 +30,16 @@ function mousePressed() {
       Encrycption.pos = { x: width / 2 + 10, y: 160 };
     }
   }
+
 }
 
+
 function handleDragging(card) {
-  if (card.mouse.dragging()) { //The card is constrained within the game window
-    widthConstraint = constrain(mouseX + card.mouse.x, card.width / 2, width - card.width / 2);
-    heightConstraint = constrain(mouseY + card.mouse.y, card.height / 2, height - card.height / 2);
-    card.position = createVector(widthConstraint, heightConstraint);
+  if (card.mouse.dragging()) {
+    card.moveTowards(
+        mouse.x + card.mouse.x,
+        mouse.y + card.mouse.y,
+        1);
     card.rotationLock = true;
   } else {
     card.vel.x = 0;
@@ -108,33 +122,100 @@ function setup() {
   decriptedplaintxt.addImage(decriptedplaintxtImg);
   decriptedplaintxt.scale = 0.50;
   cards[4] = decriptedplaintxt;
+
+
+  plaintext.pos = { x: -100, y: -100 };
+  publicKey.pos = { x: -100, y: -100 };
+  CipheredData.pos = { x: -100, y: -100 };
+  privateKey.pos = { x: -100, y: -100 };
+  decriptedplaintxt.pos = { x: -100, y: -100 };
+  Encrycption.pos = { x: -200, y: -200 };
+
 }
 
+
 function draw() {
-  // Set up the screen 
+  // Set up the screen
   clear();
   background("white");
+  if(screen === 0){
+    showStartScreen();
 
-  if (screen === 0) {
+  }else if(screen === 1){
+    showInstructionScreen();
+
+
+  } else if (screen === 2) {
     for (let card of cards) {
       handleDragging(card);
       snapToCenter(card);
     }
     //Check if we win!!!
     if (
-      dist(plaintext.x, plaintext.y, center1.x, center1.y) < 1 &&
-      dist(publicKey.x, publicKey.y, center2.x, center2.y) < 1 &&
-      dist(CipheredData.x, CipheredData.y, center3.x, center3.y) < 1 &&
-      dist(privateKey.x, privateKey.y, center4.x, center4.y) < 1 &&
-      dist(decriptedplaintxt.x, decriptedplaintxt.y, center5.x, center5.y) < 1
+        dist(plaintext.x, plaintext.y, center1.x, center1.y) < 1 &&
+        dist(publicKey.x, publicKey.y, center2.x, center2.y) < 1 &&
+        dist(CipheredData.x, CipheredData.y, center3.x, center3.y) < 1 &&
+        dist(privateKey.x, privateKey.y, center4.x, center4.y) < 1 &&
+        dist(decriptedplaintxt.x, decriptedplaintxt.y, center5.x, center5.y) < 1
     ) {
       console.log("you win!");
       showScreenWin();
-      screen = 1;
+      screen = 3;
     }
-  } else if (screen === 1) {
+  } else if (screen === 3) {
     showScreenWin();
   }
+}
+
+function showStartScreen(){
+  plaintext.pos = { x: -100, y: -100 };
+  publicKey.pos = { x: -100, y: -100 };
+  CipheredData.pos = { x: -100, y: -100 };
+  privateKey.pos = { x: -100, y: -100 };
+  decriptedplaintxt.pos = { x: -100, y: -100 };
+  Encrycption.pos = { x: -200, y: -200 };
+  background("pink");
+
+  // Set text properties
+  fill(255); // White color
+  textSize(32); // Font size
+  textAlign(CENTER, CENTER); // Text alignment
+  text("Start Screen\n\n", width / 2, height / 2 - 100);
+
+// Instructions button
+  fill(255);
+  rect(width / 2 - 70, height / 2 + 50, 150, 40);
+  fill(0);
+  textSize(20);
+  text("Instructions", width / 2, height / 2 + 70);
+
+
+
+
+}
+
+
+function showInstructionScreen(){
+  plaintext.pos = { x: -100, y: -100 };
+  publicKey.pos = { x: -100, y: -100 };
+  CipheredData.pos = { x: -100, y: -100 };
+  privateKey.pos = { x: -100, y: -100 };
+  decriptedplaintxt.pos = { x: -100, y: -100 };
+  Encrycption.pos = { x: -200, y: -200 };
+  background("blue");
+
+  // Set text properties
+  fill(255); // White color
+  textSize(32); // Font size
+  textAlign(CENTER, CENTER); // Text alignment
+  text("Instructions!\n\n", width / 2, height / 2 - 100);
+
+  // Begin button
+  fill(255);
+  rect(width / 2 - 50, height / 2 + 50, 100, 40);
+  fill(0);
+  textSize(20);
+  text("Begin", width / 2, height / 2 + 70);
 }
 
 function showScreenWin() {
